30ef0acbc054c4320fc52e965eca04d6c7a00d40
Rohit Mengji <rohitmengjih@gmail.com>
2023-02-18 22:45:25 +0530

    added localstorage

--- src/components/note/Note.js
+++ src/components/note/Note.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import Navbar from "../navbar/Navbar";
 import NoteForm from "../noteform/NoteForm";
 import NoteList from "../notelist/NoteList";
@@ -7,8 +7,22 @@ const NotesApp = () => {
   const [notes, setNotes] = useState([]);
   const [editingNoteId, setEditingNoteId] = useState(null);
 
+  // This will check if there is any data stored in the local storage and
+  // update the notes state accordingly when the component mounts.
+
+  useEffect(() => {
+    const storedNotes = JSON.parse(localStorage.getItem("notes"));
+    // console.log(storedNotes);
+    if (storedNotes) {
+      setNotes(storedNotes);
+    }
+  }, []);
+
   const addNote = (newNote) => {
-    setNotes([...notes, newNote]);
+    const updatedNotes = [...notes, newNote];
+    setNotes(updatedNotes);
+    //adds data to the user local storage
+    localStorage.setItem("notes", JSON.stringify(updatedNotes));
   };
 
   const editNote = (updatedNote) => {
@@ -20,19 +34,22 @@ const NotesApp = () => {
       }
     });
     setNotes(updatedNotes);
+    // this is stored after edited input in local storage
+    localStorage.setItem("notes", JSON.stringify(updatedNotes));
     setEditingNoteId(null);
   };
 
   const deleteNote = (noteId) => {
     const newNotes = notes.filter((note) => note.id !== noteId);
     setNotes(newNotes);
+    localStorage.setItem("notes", JSON.stringify(newNotes));
   };
 
   return (
     <div>
       {/* display navbar */}
       <Navbar />
-      <h3 className="text-center mt-2" > Notes App</h3>
+      <h3 className="text-center mt-2"> Notes App</h3>
       <NoteForm addNote={addNote} />
       {notes.length > 0 ? (
         <NoteList
